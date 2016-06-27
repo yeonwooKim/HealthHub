bd02361a6b30bbc72b6946fded7227bdd85d09a9
yeonwooKim <nokjiwon96@gmail.com>
2016-06-27 10:17:25 +0900

    Add skeleton for pnacl-ed gdcm project
    * This is a slightly revised copy of pepper_49/examples directory.
    * Adds gdcm library in demo/webPACS (and Makefile).
    * Tries compile but fails with cmake related errors.

---
+++ demo/webPACS/pnacl/Release/gdcm_library/Source/Common/gdcmASN1.d
@@ -0,0 +1,10 @@
+pnacl/Release/gdcm_library/Source/Common/gdcmASN1.o: \
+  gdcm_library/Source/Common/gdcmASN1.cxx \
+  gdcm_library/Source/Common/gdcmASN1.h \
+  gdcm_library/Source/Common/gdcmTypes.h \
+  gdcm_library/Source/Common/gdcmConfigure.h \
+  gdcm_library/Source/Common/gdcmWin32.h \
+  gdcm_library/Source/Common/gdcmLegacyMacro.h \
+  gdcm_library/Source/Common/gdcmException.h \
+  gdcm_library/Source/Common/gdcmTrace.h \
+  gdcm_library/Source/Common/gdcmSystem.h
