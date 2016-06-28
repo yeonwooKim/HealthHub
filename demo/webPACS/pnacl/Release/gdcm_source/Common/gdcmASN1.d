4a3cc0a8456abff5dc011c2bfe1462ab3cb31130
yeonwooKim <nokjiwon96@gmail.com>
2016-06-28 10:14:07 +0900

    Remove error-causing directory temporarily
    * Helps fast make by removing unnecessary example projects.
    * File IO, Graphics2D, socket, URL loader, websocket remains, the rest is removed.
    * webPACS directory removed temporarily to compile sample webpage.

--- demo/webPACS/pnacl/Release/gdcm_source/Common/gdcmASN1.d
+++
@@ -1,6 +0,0 @@
-pnacl/Release/gdcm_source/Common/gdcmASN1.o: \
-  gdcm_source/Common/gdcmASN1.cxx gdcm_source/Common/gdcmASN1.h \
-  gdcm_source/Common/gdcmTypes.h gdcm_source/Common/gdcmConfigure.h \
-  gdcm_source/Common/gdcmWin32.h gdcm_source/Common/gdcmLegacyMacro.h \
-  gdcm_source/Common/gdcmException.h gdcm_source/Common/gdcmTrace.h \
-  gdcm_source/Common/gdcmSystem.h
